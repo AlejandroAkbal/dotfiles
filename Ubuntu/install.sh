bdc870542bcf17db43d842cda3e45457b416d847
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-20 13:39:02 +0100

    Added ubuntu script

---
+++ Ubuntu/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt install nodejs npm python3-pip git gdebi-core
+
+# Install apps
+sudo snap install --classic code
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Set python
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
