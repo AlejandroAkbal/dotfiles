3e599cec95c86d9cec31924a0be81cd8db7dd71e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-10-25 13:49:34 +0100

    feat(after everything): add ssh-agent startup

--- Windows 10/After everything/index.bat
+++ Windows 10/After everything/index.bat
@@ -4,4 +4,8 @@ echo Configuring git to have lf endings
 git config --global core.autocrlf false
 git config --global core.eol lf
 
+
+echo Configuring windows to autostart ssh-agent
+Set-Service ssh-agent -StartupType Automatic
+
 echo Done
