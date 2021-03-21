9369e15a8a769d139b540c462ff3dc6c5dc45d70
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-21 10:55:04 +0100

    refactor: simplify docker installation

--- Ubuntu/scripts/apps/docker.sh
+++ Ubuntu/scripts/apps/docker.sh
@@ -4,32 +4,16 @@ APP_NAME="Docker"
 
 printf "Installing $APP_NAME"
 
-sudo apt update
-
-sudo apt install -y \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-sudo apt update
-
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-sudo docker run hello-world
+sudo apt install -y docker.io
 
 # Enable services
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+sudo systemctl enable --now docker
 
 # Add `docker` group to current user
 sudo usermod -aG docker $USER
+
+sudo apt install -y docker-compose
+
+sudo docker run hello-world
 
 printf "\n$APP_NAME successfully installed\n"
