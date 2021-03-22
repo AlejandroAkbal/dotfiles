497dc2672dadacdc129f94af4798ed8bf4c24798
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-22 19:50:34 +0100

    refactor: output to the same file

--- Ubuntu/scripts/apps/realvnc-server.sh
+++ Ubuntu/scripts/apps/realvnc-server.sh
@@ -6,10 +6,10 @@ printf "Installing $APP_NAME"
 
 cd /tmp
 
-wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.2-Linux-x86.deb
+wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.2-Linux-x86.deb -O VNC-Server.deb
 
-sudo apt install -y ./VNC-Server-6.7.2-Linux-x86.deb
+sudo apt install -y ./VNC-Server.deb
 
-rm VNC-Server-6.7.2-Linux-x86.deb
+rm VNC-Server.deb
 
 printf "\n$APP_NAME successfully installed\n"
--- Ubuntu/scripts/apps/realvnc-viewer.sh
+++ Ubuntu/scripts/apps/realvnc-viewer.sh
@@ -6,10 +6,10 @@ printf "Installing $APP_NAME"
 
 cd /tmp
 
-wget https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.20.529-Linux-x86.deb
+wget https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.20.529-Linux-x86.deb -O VNC-Viewer.deb
 
-sudo apt install -y ./VNC-Viewer-6.20.529-Linux-x86.deb
+sudo apt install -y ./VNC-Viewer.deb
 
-rm VNC-Viewer-6.20.529-Linux-x86.deb
+rm VNC-Viewer.deb
 
 printf "\n$APP_NAME successfully installed\n"
