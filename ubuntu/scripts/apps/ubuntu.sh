3b9fb7e2969ee43915c3de60d4c06cf2497586d2
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-02-15 15:59:49 +0100

    feat: add time synchronization fix

--- ubuntu/scripts/apps/ubuntu.sh
+++ ubuntu/scripts/apps/ubuntu.sh
@@ -6,4 +6,7 @@ printf "Making Ubuntu changes"
 sudo systemctl disable cups --now
 sudo systemctl disable cups-browsed --now
 
+# Fix time difference in dual boot
+timedatectl set-local-rtc 1 --adjust-system-clock
+
 printf "Done making changes"
