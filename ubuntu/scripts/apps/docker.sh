ada0a0c1328ce3e4946367bb1d6bf1e8bbbce9a7
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-02-16 13:52:13 +0100

    chore: update script

--- ubuntu/scripts/apps/docker.sh
+++ ubuntu/scripts/apps/docker.sh
@@ -5,37 +5,22 @@ APP_NAME="Docker"
 printf "Installing $APP_NAME"
 
 # Docker
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
 
-sudo apt update
+curl -fsSL https://get.docker.com -o get-docker.sh
 
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo sh get-docker.sh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+rm get-docker.sh
 
-echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-sudo apt update
-
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+dockerd-rootless-setuptool.sh install --force
 
 # Docker compose
+# https://docs.docker.com/compose/install
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Configuration
-
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
-sudo groupadd docker
-
-sudo usermod -aG docker $USER
-
-newgrp docker
 
 # Test
 docker run hello-world
