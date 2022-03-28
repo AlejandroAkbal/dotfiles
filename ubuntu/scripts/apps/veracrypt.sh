da6d2bd9e7bf4278e51ef36835972fa01971e5aa
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-03-28 20:29:39 +0200

    chore: update veracrypt

--- ubuntu/scripts/apps/veracrypt.sh
+++ ubuntu/scripts/apps/veracrypt.sh
@@ -4,7 +4,7 @@ APP_NAME="VeraCrypt"
 
 printf "Installing $APP_NAME"
 
-curl -L "https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb" -o ./veracrypt.deb
+curl -L "https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-20.04-amd64.deb" -o ./veracrypt.deb
 
 sudo apt install -y ./veracrypt.deb
 
